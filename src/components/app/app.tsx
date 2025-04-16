5e6f643e860d084fa225996cca205316fa6662a2
Sergey Malyuga <sergey@malyuga.ru>
2025-04-16 16:16:39 +0300

    Добавить маршрутизацию

---
+++ src/components/app/app.tsx
@@ -0,0 +1,35 @@
+import MainScreen from '../../pages/main-screen/main-screen.tsx';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen.tsx';
+import {AppRoute, AuthorizationStatus} from '../../const.ts';
+import LoginScreen from '../../pages/login-screen/login-screen.tsx';
+import OfferScreen from '../../pages/offer-screen/offer-screen.tsx';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen.tsx';
+import PrivateRoute from '../private-route/private-route.tsx';
+
+type AppScreenProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppScreenProps): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root}>
+          <Route index element={<MainScreen offersCount={offersCount}/>}/>
+          <Route path={AppRoute.Login} element={<LoginScreen/>}/>
+          <Route path={AppRoute.Favorites} element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            ><FavoritesScreen/>
+            </PrivateRoute>
+          }
+          />
+          <Route path={`${AppRoute.Offer}/:id`} element={<OfferScreen/>}/>
+        </Route>
+        <Route path='*' element={<NotFoundScreen/>}/>
+      </Routes>
+    </BrowserRouter>);
+}
+
+export default App;
